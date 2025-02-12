9db052d8396d250176bc82394716a54237d25156
gais3ric <dmrmkn01@gmail.com>
2025-02-12 21:37:29 +0300

    Update Board.java
    Вырезан сайдбар к чертям собачьим

--- src/main/java/tk/shanebee/hg/game/Board.java
+++ src/main/java/tk/shanebee/hg/game/Board.java
@@ -34,28 +34,6 @@ public class Board {
     private final Team team;
     private final String[] entries = new String[]{"&1&r", "&2&r", "&3&r", "&4&r", "&5&r", "&6&r", "&7&r", "&8&r", "&9&r", "&0&r", "&a&r", "&b&r", "&c&r", "&d&r", "&e&r"};
 
-    @SuppressWarnings("ConstantConditions")
-    public Board(Game game) {
-        this.game = game;
-        this.plugin = game.plugin;
-        scoreboard = Bukkit.getScoreboardManager().getNewScoreboard();
-        board = scoreboard.registerNewObjective("Board", "dummy", "Board");
-        board.setDisplaySlot(DisplaySlot.SIDEBAR);
-        board.setDisplayName(" ");
-
-        for (int i = 0; i < 15; i++) {
-            lines[i] = scoreboard.registerNewTeam("line" + (i + 1));
-        }
-
-        for (int i = 0; i < 15; i++) {
-            lines[i].addEntry(Util.getColString(entries[i]));
-        }
-        team = scoreboard.registerNewTeam("game-team");
-
-        if (Config.hideNametags) {
-            team.setOption(Option.NAME_TAG_VISIBILITY, OptionStatus.NEVER);
-        }
-    }
 
     Team registerTeam(String name) {
         Team team = scoreboard.registerNewTeam(name);
